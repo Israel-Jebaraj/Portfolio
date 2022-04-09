9f780374bdbb98a9442209af15ed142614ff7772
Israel Jebaraj <israeljebaraj.c@syncfusion.com>
2022-04-09 15:12:30 +0530

    userId based rendering

--- user.js
+++ user.js
@@ -1,9 +1,16 @@
-window.userName="";
+window.Id=0;
+window.userDetails=[
+    {
+        name:"Siva sankar"
+    },
+    {
+        name:"Israel Jebaraj"
+    }];
 $(document).ready(()=>{
-    userName=window.prompt("Enter you name");
-    this.changeUserName();
+    userId=parseInt(window.prompt("Enter you name"))-1;
+    this.changeUserName(userId);
 });
-function changeUserName(){
-    $(".home .home-content .image-content .sample-img").html(userName);
-    $(".home .home-content .description-content p span.name").html(userName);
+function changeUserName(userId){
+    $(".home .home-content .image-content .sample-img").html(userDetails[userId].name);
+    $(".home .home-content .description-content p span.name").html(userDetails[userId].name);
 }
