cf22daca4cf789b91592bbdfbb3ba7633dc0e20a
Israel Jebaraj <israeljebaraj.c@syncfusion.com>
2022-04-10 13:03:34 +0530

    Added description and some CSS changes

--- user.js
+++ user.js
@@ -1,10 +1,14 @@
 window.Id=0;
 window.userDetails=[
     {
-        name:"Siva sankar"
+        name:"Siva sankar",
+        resumeDescription:"I've 1 year experiance at syncfusion."+ 
+        " Having konwledge in web development. Also I'm egar to learn  new technologies.",
     },
     {
-        name:"Israel Jebaraj"
+        name:"Israel Jebaraj",
+        resumeDescription:"I've 1 year experiance at syncfusion."+ 
+        " Having konwledge in web development. Also I'm egar to learn  new technologies.",
     }];
 $(document).ready(()=>{
     userId=parseInt(window.prompt("Enter id"))-1;
@@ -13,4 +17,5 @@ $(document).ready(()=>{
 function changeUserName(userId){
     $(".home .home-content .image-content .sample-img").html(userDetails[userId].name);
     $(".home .home-content .description-content p span.name").html(userDetails[userId].name);
+    $(".home .home-content .description-content p span.description-intro").html(userDetails[userId].resumeDescription);     
 }
