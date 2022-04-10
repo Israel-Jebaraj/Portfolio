0eaccfb5d10671e44757cec06e73ec9bc075b154
Israel Jebaraj <israeljebaraj.c@syncfusion.com>
2022-04-10 14:02:48 +0530

    user.js commit details

--- user.js
+++ user.js
@@ -2,20 +2,20 @@ window.Id=0;
 window.userDetails=[
     {
         name:"Siva sankar",
-        resumeDescription:"I've 1 year experiance at syncfusion."+ 
+        resumeDescription:"I've 1 year experiance in a product based company."+ 
         " Having konwledge in web development. Also I'm egar to learn  new technologies.",
     },
     {
         name:"Israel Jebaraj",
-        resumeDescription:"I've 1 year experiance at syncfusion."+ 
+        resumeDescription:"I've 1 year experiance in a product based company."+ 
         " Having konwledge in web development. Also I'm egar to learn  new technologies.",
     }];
 $(document).ready(()=>{
-    userId=parseInt(window.prompt("Enter id"))-1;
-    this.changeUserName(userId);
+    Id=parseInt(window.prompt("Enter id"))-1;
+    this.changeUserName();
 });
-function changeUserName(userId){
-    $(".home .home-content .image-content .sample-img").html(userDetails[userId].name);
-    $(".home .home-content .description-content p span.name").html(userDetails[userId].name);
-    $(".home .home-content .description-content p span.description-intro").html(userDetails[userId].resumeDescription);     
+function changeUserName(){
+    $(".home .home-content .image-content .sample-img").html(userDetails[Id].name);
+    $(".home .home-content .description-content p span.name").html(userDetails[Id].name);
+    $(".home .home-content .description-content p span.description-intro").html(userDetails[Id].resumeDescription);     
 }
